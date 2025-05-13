e13d90cfe0caf2ef6d40a623d7f3d7861a8defcd
Jean Ramos <jean_henriquez@hotmail.com>
2025-05-12 22:09:00 -0600

    new carpool page with better design

--- lib/carpool_by_resort/presentation/carpool_posts.dart
+++ lib/carpool_by_resort/presentation/carpool_posts.dart
@@ -1,164 +1,183 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
-import 'package:intl/intl.dart';
-import 'package:powder_pool/app_bars/presentation/auth_app_bar.dart';
-import 'package:powder_pool/carpool_by_resort/application/carpools_by_resort_provider.dart';
-import 'package:powder_pool/loading/presentation/loading_welcome.dart';
-import 'package:powder_pool/models/carpool_model.dart';
-import 'package:powder_pool/models/domain/uuid.dart';
-import 'package:powder_pool/router/domain/routes.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:go_router/go_router.dart';
+// import 'package:intl/intl.dart';
+// import 'package:powder_pool/app_bars/presentation/auth_app_bar.dart';
+// import 'package:powder_pool/carpool_by_resort/application/carpools_by_resort_provider.dart';
+// import 'package:powder_pool/loading/presentation/loading_welcome.dart';
+// import 'package:powder_pool/models/carpool_model.dart';
+// import 'package:powder_pool/models/domain/uuid.dart';
+// import 'package:powder_pool/resorts/presentation/card_button.dart';
+// import 'package:powder_pool/resorts/presentation/resorts_list.dart';
+// import 'package:powder_pool/router/domain/routes.dart';
 
-class CarpoolsByResortPage extends ConsumerWidget {
-  final Uuid resortId;
-  const CarpoolsByResortPage({super.key, required this.resortId});
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    var carpoolPosts = ref.watch(carpoolsByResortId(resortId));
-    return carpoolPosts.when(
-      data: (carpoolPosts) {
-        return CarpoolsByResortLoaded(carpoolPosts: carpoolPosts);
-      },
-      error: (error, stackTrace) {
-        return Center(child: Text('Error: $error'));
-      },
-      loading: () => LoadingWelcome(),
-    );
-  }
-}
+// class CarpoolsByResortPage extends ConsumerWidget {
+//   final Uuid resortId;
+//   const CarpoolsByResortPage({super.key, required this.resortId});
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     var carpoolPosts = ref.watch(carpoolsByResortId(resortId));
+//     return carpoolPosts.when(
+//       data: (carpoolPosts) {
+//         return CarpoolsByResortLoaded(carpoolPosts: carpoolPosts);
+//       },
+//       error: (error, stackTrace) {
+//         return Center(child: Text('Error: $error'));
+//       },
+//       loading: () => LoadingWelcome(),
+//     );
+//   }
+// }
 
-class CarpoolsByResortLoaded extends ConsumerStatefulWidget {
-  final List<Carpool> carpoolPosts;
-  const CarpoolsByResortLoaded({super.key, required this.carpoolPosts});
+// class CarpoolsByResortLoaded extends ConsumerStatefulWidget {
+//   final List<Carpool> carpoolPosts;
+//   const CarpoolsByResortLoaded({super.key, required this.carpoolPosts});
 
-  @override
-  ConsumerState<CarpoolsByResortLoaded> createState() =>
-      CarpoolsByResortState();
-}
+//   @override
+//   ConsumerState<CarpoolsByResortLoaded> createState() =>
+//       CarpoolsByResortState();
+// }
 
-class CarpoolsByResortState extends ConsumerState<CarpoolsByResortLoaded> {
-  final Set<int> requestedIndices = {};
+// class CarpoolsByResortState extends ConsumerState<CarpoolsByResortLoaded> {
+//   final Set<int> requestedIndices = {};
 
-  @override
-  Widget build(BuildContext context) {
-    final List<Carpool> carpoolPosts = widget.carpoolPosts;
+//   @override
+//   Widget build(BuildContext context) {
+//     List<Carpool> carpoolPosts = widget.carpoolPosts;
+//     carpoolPosts.sort((a, b) {
+//       // Sort by departure date first, then by departure time
+//       if (a.departureDate != null && b.departureDate != null) {
+//         return a.departureDate!.compareTo(b.departureDate!);
+//       } else if (a.departureDate == null && b.departureDate == null) {
+//         return 0;
+//       } else if (a.departureDate == null) {
+//         return 1;
+//       } else {
+//         return -1;
+//       }
+//     });
+//     final textTheme = Theme.of(
+//       context,
+//     ).textTheme.apply(displayColor: Theme.of(context).colorScheme.onSurface);
 
-    final avatarAsset = 'assets/images/resorts/deer_valley.png';
-    return Scaffold(
-      appBar: AuthAppBar(),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(16),
-        itemCount: carpoolPosts.length,
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 300,
-          mainAxisSpacing: 16,
-          crossAxisSpacing: 16,
-          childAspectRatio: 0.85,
-        ),
-        itemBuilder: (context, index) {
-          final post = carpoolPosts[index];
-          final isRequested = requestedIndices.contains(index);
+//     // Set the default number of columns to 3.
+//     int columnsCount = 2;
 
-          return Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(16),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black12,
-                  blurRadius: 6,
-                  offset: Offset(0, 2),
-                ),
-              ],
-            ),
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  backgroundImage: AssetImage(avatarAsset),
-                  radius: 32,
-                ),
-                const SizedBox(height: 12),
-                Text(
-                  post.name ?? '',
-                  style: const TextStyle(fontWeight: FontWeight.w600),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  'Organized by: ',
-                  style: TextStyle(color: Colors.grey.shade700),
-                ),
-                const SizedBox(height: 4),
-                Text(
-                  '${post.creator?.firstName ?? ''} ${post.creator?.lastName ?? ''}',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 4),
-                Text(
-                  post.description ?? '',
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(fontSize: 13, color: Colors.black87),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                const SizedBox(height: 16),
-                Text(
-                  'Departs on',
-                  style: const TextStyle(fontSize: 14, color: Colors.black38),
-                ),
-                Text(
-                  post.departureDate != null
-                      ? DateFormat('MMM dd, yyyy').format(post.departureDate!)
-                      : '',
-                  style: const TextStyle(fontSize: 13, color: Colors.black54),
-                ),
-                Text(
-                  'at ${post.departureTime}',
-                  style: const TextStyle(fontSize: 12, color: Colors.black38),
-                ),
-                const SizedBox(height: 16),
-                TextButton(
-                  onPressed:
-                      isRequested
-                          ? null
-                          : () {
-                            context.goNamed(
-                              Routes.carpoolConversation.name,
-                              pathParameters: {'carpoolId': post.id.toString()},
-                            );
-                          },
-                  child: Text('Join Now!'),
-                ),
-              ],
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
+//     double iconSize = 45;
 
-class Post {
-  final String name;
-  final String title;
-  final String subtitle;
-  final String date;
-  final String departureTime;
-  final String avatarAsset;
+//     if (ResponsiveUtils.isMobile(context)) {
+//       columnsCount = 1;
+//       iconSize = 30;
+//     } else if (ResponsiveUtils.isDesktop(context)) {
+//       columnsCount = 4;
+//       iconSize = 50;
+//     }
 
-  Post({
-    required this.name,
-    required this.title,
-    required this.subtitle,
-    required this.date,
-    required this.departureTime,
-    required this.avatarAsset,
-  });
-}
+//     return Scaffold(
+//       appBar: AuthAppBar(),
+//       // d8e4eb hex
+//       body: Container(
+//         color: const Color.fromARGB(255, 216, 231, 240),
+//         child: GridView.builder(
+//           padding: const EdgeInsets.all(10),
+//           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+//             crossAxisCount: columnsCount,
+//             childAspectRatio: 3 / 2,
+//             crossAxisSpacing: 20,
+//             mainAxisSpacing: 10,
+//           ),
+//           itemCount: carpoolPosts.length,
+//           itemBuilder: (BuildContext context, int index) {
+//             return CarpoolCard(carpool: carpoolPosts[index]);
+//           },
+//           // Set the grid view to shrink wrap its contents.
+//           shrinkWrap: true,
+//           // Disable scrolling in the grid view.
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class CarpoolCard extends StatelessWidget {
+//   final Carpool carpool;
+
+//   const CarpoolCard({required this.carpool, super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final imagePath = "assets/images/resorts/deer_valley.png";
+
+//     return Card(
+//       color: Colors.white,
+//       elevation: 3,
+//       child: Column(
+//         mainAxisSize: MainAxisSize.min, // 🔑 only wrap content vertically
+//         crossAxisAlignment: CrossAxisAlignment.stretch,
+//         children: [
+//           // Expanded(
+//           //   // Add an image to each card.
+//           //   child: Image.asset(imagePath, fit: BoxFit.cover),
+//           // ),
+//           const SizedBox(height: 10),
+//           Padding(
+//             padding: const EdgeInsets.symmetric(horizontal: 10),
+
+//             child: Column(
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               mainAxisSize: MainAxisSize.min, // 🔑 also on inner column
+//               children: [
+//                 MyTextStyle(
+//                   name: carpool.name ?? '',
+//                   style: const TextStyle(
+//                     fontSize: 16,
+//                     fontWeight: FontWeight.bold,
+//                   ),
+//                 ),
+//                 const SizedBox(height: 2),
+//                 MyTextStyle(
+//                   name:
+//                       "Organized by: ${carpool.creator?.firstName ?? ''} ${carpool.creator?.lastName ?? ''}",
+//                   style: const TextStyle(fontSize: 14, color: Colors.black54),
+//                 ),
+//                 const SizedBox(height: 10),
+//                 MyTextStyle(
+//                   name: carpool.description ?? '',
+//                   style: const TextStyle(fontSize: 14, height: 1.3),
+//                 ),
+//                 const SizedBox(height: 5),
+//                 MyTextStyle(
+//                   name:
+//                       carpool.departureDate != null
+//                           ? DateFormat(
+//                             'MMM dd, yyyy',
+//                           ).format(carpool.departureDate!)
+//                           : '',
+//                   style: const TextStyle(fontSize: 14, color: Colors.black54),
+//                 ),
+//                 const SizedBox(height: 2),
+//                 MyTextStyle(
+//                   name: carpool.departureTime ?? '',
+//                   style: const TextStyle(fontSize: 14, color: Colors.black54),
+//                 ),
+
+//                 const SizedBox(height: 8),
+//                 Align(
+//                   alignment: Alignment.centerRight,
+//                   child: JoinCarpoolLink(
+//                     onTap: () {
+//                       context.goNamed(
+//                         Routes.carpoolConversation.name,
+//                         pathParameters: {'carpoolId': carpool.id.toString()},
+//                       );
+//                     },
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
