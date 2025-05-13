00e4f76209088e564498d44aca498269533f410e
Jean Ramos <jean_henriquez@hotmail.com>
2025-05-13 00:40:00 -0600

    correct url

--- lib/api_client/data/api_client.dart
+++ lib/api_client/data/api_client.dart
@@ -7,7 +7,7 @@ import 'package:powder_pool/models/domain/user_model.dart';
 class ApiClient {
   static const _tokenKey = 'jwt_token';
   String token = '';
-  static const String baseUrl = 'https://powderpool.com';
+  static const String baseUrl = 'https://powder-pool.com';
 
   ApiClient();
 
