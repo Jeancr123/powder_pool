1debdc347bb892e7b6d8138b9f95551c879a919a
Jean Ramos <jean_henriquez@hotmail.com>
2025-05-12 22:09:11 -0600

    new home page, better UI

--- lib/home/presentation/home.dart
+++ lib/home/presentation/home.dart
@@ -2,185 +2,241 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:powder_pool/app_bars/presentation/auth_app_bar.dart';
 import 'package:powder_pool/login/presentation/animated_button.dart';
+import 'package:powder_pool/resorts/presentation/resorts_list.dart';
 import 'package:powder_pool/router/domain/routes.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
-  AnimationController? _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 1000),
-    );
-
-    _controller?.forward();
-  }
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(
+      context,
+    ).textTheme.apply(displayColor: Theme.of(context).colorScheme.onSurface);
+
+    var width = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AuthAppBar(),
-      backgroundColor: Colors.white,
-      body: ListView(
-        children: [
-          // Hero Image
-          Image.asset(
-            'assets/images/background.png',
-            height: 200,
-            width: double.infinity,
-            fit: BoxFit.cover,
-          ),
-
-          // Slogan and Description
-          Container(
-            color: const Color(0xFFF8F8F8),
-            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
-            child: Column(
-              children: const [
-                Text(
-                  'ALL ACCESS. ALL MOUNTAIN. ALL SNOW.',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w700,
-                    letterSpacing: 1.5,
-                    color: Color(0xFF333333),
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 24),
-                Text(
-                  '''PowderPool is the smart way to carpool to your favorite ski resorts. Whether you're chasing first tracks, saving on gas and parking, or just looking for good company on the drive up, PowderPool makes it easy to match with riders who fit your vibe, your schedule, and your mountain goals.''',
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Color(0xFF555555),
-                    // height: 1.5,
-                    fontWeight: FontWeight.w500,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
-          ),
-
-          // HOW WE HELP YOU Section
-          Container(
-            color: Colors.white,
-            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 24),
+      body: Container(
+        height: double.infinity,
+        // color: const Color(0xFFF5F5F5),
+        // ✅ Center the inner container
+        child: SingleChildScrollView(
+          child: Center(
             child: Column(
               children: [
-                const Text(
-                  'HOW WE HELP YOU',
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.w800),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 16),
-                const Text(
-                  'These are just some of the many reasons to use PowderPool. '
-                  'With PowderPool, you can find the best carpooling options that fit your time, your vibe, and your wallet.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 14, color: Colors.black87),
-                ),
-                const SizedBox(height: 32),
+                Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 100),
+                      Center(
+                        child: Container(
+                          width: width,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: MediaQuery.of(context).size.width * .6,
+                                child: Padding(
+                                  padding: EdgeInsets.symmetric(
+                                    horizontal: 100,
+                                  ),
+                                  child: Text(
+                                    'CONNECTING YOU TO OTHER SKIERS',
+                                    style: textTheme.headlineLarge?.copyWith(
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 72,
+                                      color: Color.fromRGBO(58, 58, 58, 1),
+                                    ),
+                                    textAlign: TextAlign.left,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(width: 50),
+                              Container(
+                                width: MediaQuery.of(context).size.width * .30,
+                                child: Text(
+                                  'Stay connected with other skiers and travel together to the most beautiful resorts in Utah. Ride together, save money, and make new friends.',
+                                  style: textTheme.headlineMedium?.copyWith(
+                                    fontWeight: FontWeight.w300,
+                                    fontSize: 18,
+                                    letterSpacing:
+                                        textTheme.headlineLarge?.letterSpacing,
 
-                // Responsive Offer Cards
-                Wrap(
-                  spacing: 20,
-                  runSpacing: 32,
-                  alignment: WrapAlignment.center,
-                  children: const [
-                    OfferCard(
-                      image: 'assets/images/pros/planet.png',
-                      title: 'CARPOOL AND SAVE THE PLANET',
-                      subtitle: 'Want to keep the mountains snowy?',
-                    ),
-                    OfferCard(
-                      image: 'assets/images/pros/parking.png',
-                      title: 'FILL THE CAR, WIN THE PARKING LOT',
-                      subtitle:
-                          'Don\'t have enough people to park at the resort?',
-                    ),
-                    OfferCard(
-                      image: 'assets/images/pros/save.png',
-                      title: 'SPLIT THE COST: GAS AND PARKING',
-                      subtitle: 'Want to save \$?',
-                    ),
-                    OfferCard(
-                      image: 'assets/images/pros/comfort.png',
-                      title: 'RIDE COMFY AND SAFE',
-                      subtitle: 'Hate the bus?',
-                    ),
-                    OfferCard(
-                      image: 'assets/images/pros/friends.png',
-                      title: 'RIDE WITH FRIENDS OR MAKE NEW ONES',
-                      subtitle: 'Tired of riding alone?',
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 32),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * .4,
-                  child: AnimatedButton(
-                    controller: _controller!,
-                    label: 'Register Now!',
-                    onTap: () {
-                      context.goNamed(Routes.signUp.name);
-                    },
+                                    color: Color.fromRGBO(58, 58, 58, 1),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 80),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        child: Row(
+                          children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width * .6,
+                              height: 450,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage(
+                                    "assets/images/resorts/deer_valley.png",
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 50),
+                            Container(
+                              width: MediaQuery.of(context).size.width * .3,
+                              child: Column(
+                                children: [
+                                  Text(
+                                    'GROWING COMMUNITY',
+                                    style: textTheme.headlineMedium?.copyWith(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 18,
+                                      color: Color(0xFF116e74),
+                                    ),
+                                    textAlign: TextAlign.left,
+                                  ),
+                                  SizedBox(height: 15),
+
+                                  Text(
+                                    'Join the growing PowderPool community and visit more resorts together. Find friends with discounts, skills, and more. PowderPool supports all the major resots in Utah, and we continue to grow and expand.',
+                                    style: textTheme.bodyLarge?.copyWith(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 16,
+                                      letterSpacing:
+                                          textTheme.bodySmall?.letterSpacing,
+                                      color: Color.fromRGBO(58, 58, 58, 1),
+                                    ),
+                                  ),
+                                  SizedBox(height: 50),
+                                  Text(
+                                    'WHEN AND HOW IS UP TO YOU',
+                                    style: textTheme.headlineMedium?.copyWith(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 18,
+                                      color: Color(0xFF116e74),
+                                    ),
+                                    textAlign: TextAlign.left,
+                                  ),
+                                  SizedBox(height: 15),
+                                  Text(
+                                    'It\'s okay to be nervous about riding with strangers. But with PowderPool, you can choose who you ride with and when. We\'re here to help you navigate meeting new skiers and connect through carpooling.',
+                                    style: textTheme.bodyLarge?.copyWith(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 16,
+                                      letterSpacing:
+                                          textTheme.bodySmall?.letterSpacing,
+                                      color: Color.fromRGBO(58, 58, 58, 1),
+                                    ),
+                                  ),
+
+                                  SizedBox(height: 50),
+                                  Text(
+                                    'SAVE THE PLANET',
+                                    style: textTheme.headlineMedium?.copyWith(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 18,
+                                      color: Color(0xFF116e74),
+                                    ),
+                                    textAlign: TextAlign.left,
+                                  ),
+                                  SizedBox(height: 15),
+
+                                  Text(
+                                    'What better way to help the planet and keep the snow going than to carpool? Save the planet by reducing the number of cars on the road and in the canyons, and by encouraging others to clean up the environment.',
+                                    style: textTheme.bodyLarge?.copyWith(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 16,
+                                      letterSpacing:
+                                          textTheme.bodySmall?.letterSpacing,
+                                      color: Color.fromRGBO(58, 58, 58, 1),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
+
+                Container(
+                  width: width * .9,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 100),
+                      Text(
+                        'JOIN THE COMMUNITY',
+                        style: textTheme.headlineSmall?.copyWith(
+                          fontWeight: FontWeight.w300,
+                          fontSize: 14,
+                          color: Color(0xFF116e74),
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        width: MediaQuery.of(context).size.width * .7,
+                        child: Text(
+                          'Be part of a community that shares similar passions, interests, and hobbies.',
+                          style: textTheme.headlineMedium?.copyWith(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 52,
+                            letterSpacing: textTheme.bodySmall?.letterSpacing,
+                            color: Color.fromRGBO(58, 58, 58, 1),
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        width: MediaQuery.of(context).size.width * .7,
+                        child: Text(
+                          'PowderPool helps the skiers of Utah connect and travel together. Say goodbye to standing for hours on the bus. No more paying for invasive gondolas or expensive parking fees. Instead, make new friends and enjoy the ride. Skiing is better with friends.',
+                          style: textTheme.headlineMedium?.copyWith(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 18,
+                            letterSpacing: textTheme.bodySmall?.letterSpacing,
+                            color: Color.fromRGBO(58, 58, 58, 1),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 50),
+                      const SizedBox(height: 100),
+                    ],
+                  ),
+                ),
+                Container(height: 200, color: Color(0xff116e74)),
               ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
 }
 
-class OfferCard extends StatelessWidget {
-  final String image;
-  final String title;
-  final String subtitle;
+class ResponsiveUtils {
+  // Check if the device is considered as mobile based on screen width.
+  static bool isMobile(BuildContext context) =>
+      MediaQuery.of(context).size.width <= 600;
 
-  const OfferCard({
-    super.key,
-    required this.image,
-    required this.title,
-    required this.subtitle,
-  });
+  // Check if the device is considered as tablet based on screen width.
+  static bool isTablet(BuildContext context) =>
+      MediaQuery.of(context).size.width > 600 &&
+      MediaQuery.of(context).size.width <= 1200;
 
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 220, // Control width for max 3 per row on large screens
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset(image, height: 80),
-          const SizedBox(height: 8),
-          Text(
-            subtitle,
-            style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w600),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 6),
-          Text(
-            title,
-            style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w700),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 10),
-        ],
-      ),
-    );
-  }
+  // Check if the device is considered as desktop based on screen width.
+  static bool isDesktop(BuildContext context) =>
+      MediaQuery.of(context).size.width > 1200;
 }
